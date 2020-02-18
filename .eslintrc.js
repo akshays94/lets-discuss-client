c9054f34cf43b1b02e84fcfb622dac9068d45fa7
Akshay Suresh <akshaysuresh1994@gmail.com>
2020-02-18 16:47:14 +0530

    FIXED: no-console switched off for prod

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,8 +8,8 @@ module.exports = {
     '@vue/standard'
   ],
   rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-console': 'off',
+    'no-debugger': 'off',
     'no-trailing-spaces': 'off',
     'no-multiple-empty-lines': 'off',
     'no-side-effects-in-computed-properties': 'off'
